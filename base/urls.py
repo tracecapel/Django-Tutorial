77e955adff920ca5412a4983a638e911b6420158
root <root@LAPTOP-UEBCFIH5>
2025-02-09 22:08:55 -0500

    2:06:22 Commit

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 urlpatterns = [
     path('',views.home, name="home"),
     path('room/<int:pk>/', views.room, name="room"),
+
+    path('create-room/', views.createRoom, name="create-room"),
+
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
 ]

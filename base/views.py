47d8e247fa14463a5cf66476a49da6c3451d5078
root <root@LAPTOP-UEBCFIH5>
2025-02-09 22:35:46 -0500

    2:22:27

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+
+
+from .models import Room, Topic
 
 from .forms import RoomForm
 
@@ -12,15 +15,43 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+   
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+
+
+        
+        
+        
+        )
+    
+
+    
+    
+
+
+    topics = Topic.objects.all()
+
+
+    room_count = rooms.count()
+
+
+
+    context = {'rooms': rooms, 'topics': topics, 'rooms_count': room_count}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
 
     room = Room.objects.get(id=pk)
-    context = {'rooms': room}
+    
+    
+    topic= Topic.objects.all()
+    
+    context = {'rooms': room, 'topics' : topic}
 
     return render(request, 'base/room.html', context)
 

d9f3d491bf82f25f74fafdabb1f3de1c90d0c552
root <root@LAPTOP-UEBCFIH5>
2025-02-16 16:07:00 -0500

    3:43:00 Commit

--- base/views.py
+++ base/views.py
@@ -27,8 +27,12 @@ def home(request):
     room_count = rooms.count()
 
 
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'rooms_count': room_count}
+
+
+
+    context = {'rooms': rooms, 'topics': topics, 'rooms_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 

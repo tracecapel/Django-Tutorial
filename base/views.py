659f9cd79d0d76b68c9e941d841a501673039227
root <root@LAPTOP-UEBCFIH5>
2025-02-16 14:26:28 -0500

    2:49:00 Commit

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import authenticate, login, logout
@@ -66,10 +66,13 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context )
 
 
-
+@login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
 
 
     if request.method == 'POST':
@@ -87,10 +90,13 @@ def updateRoom(request,pk):
 
 
 
-
+@login_required(login_url='login')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk)
-
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
+    
     if request.method == 'POST':
         room.delete()
         return redirect('home')
@@ -102,6 +108,13 @@ def deleteRoom(request,pk):
 
 
 def loginPage(request):
+    page = 'login'
+    
+    
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    
     username = None
     password = None
     
@@ -122,7 +135,7 @@ def loginPage(request):
     else: 
         messages.error(request, 'Username or password is incorrect')
     
-    context = {}
+    context = {'page' : page}
     
     
     return render(request, 'base/login_register.html', context)
@@ -132,6 +145,11 @@ def loginPage(request):
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    page = 'register' 
+    return render(request, 'base/login_register.html')
 
 
 
